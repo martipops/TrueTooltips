0334d5f89e27c94d89b607e93d98d4f27abc6b04
martipops <melonnemann@gmail.com>
2023-08-03 22:49:11 -0400

    config fix

--- Config.cs
+++ Config.cs
@@ -8,182 +8,275 @@
     {
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
-        [Header("CustomTooltipLines")]
-        [DefaultValue(true)]
+        [Header("[c/ffff00:Custom Tooltip Lines]")]
+
+        [Label("Ammo Line"),
+        Tooltip("Shows the item's ammo's name, amount and rarity, shows the mod that adds the ammo if \"Mod name next to item name\" is on and shows the ammo the item needs if the item has no ammo. \nWorks with fishing poles and tile wands."),
+        DefaultValue(true)]
         public bool ammoLine;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Velocity Line"),
+        Tooltip("Shows the item's velocity. May not always be accurate due to items shooting multiple different projectiles, projectiles changing velocity mid flight, etc.."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color velocityLine;
 
-        [DefaultValue(true)]
+        [Label("Better Price Line"),
+        Tooltip("Correct sell price for items with custom price, always visible, better color coding, cleaner. Replaces vanilla price line."),
+        DefaultValue(true)]
         public bool priceLine;
 
-        [DefaultValue(true)]
+        [Label("Better Speed Line"),
+        Tooltip("Shows attacks per second. Replaces vanilla speed line."),
+        DefaultValue(true)]
         public bool speedLine;
 
-        [DefaultValue(true)]
+        [Label("Better Knockback Line"),
+        Tooltip("Shows knockback as a number. Replaces vanilla knockback line."),
+        DefaultValue(true)]
         public bool knockbackLine;
 
-        [DefaultValue(true)]
+        [Label("Combine weapon and ammo damage"), DefaultValue(true)]
         public bool wpnPlusAmmoDmg;
 
-        [DefaultValue(true)]
+        [Label("Combine weapon and ammo knockback"),
+        Tooltip("Requires Better Knockback Line to be on."),
+        DefaultValue(true)]
         public bool wpnPlusAmmoKb;
 
-        [DefaultValue(true)]
+        [Label("Combine weapon and ammo velocity"), DefaultValue(true)]
         public bool wpnPlusAmmoVelocity;
 
-        [DefaultValue(true)]
+        [Label("Mod name next to item name"),
+        Tooltip("Shows the mod that adds the item next to the item's name."),
+        DefaultValue(true)]
         public bool modName;
 
-        [Header("Background")]
+        [Header("[c/ffff00:Background]")]
 
-        [DefaultValue(typeof(Color), "63,81,151,255")]
+        [Label("Color"), DefaultValue(typeof(Color), "63,81,151,255")]
         public Color bgColor;
 
-        [Range(0, 999),
+        [Label("Right Padding"),
+        Range(0, 999),
         DefaultValue(10)]
         public int paddingRight;
 
-        [Range(0, 999),
+        [Label("Left Padding"),
+        Range(0, 999),
         DefaultValue(10)]
         public int paddingLeft;
 
-        [Range(0, 999),
+        [Label("Top Padding"),
+        Range(0, 999),
         DefaultValue(10)]
         public int paddingTop;
 
-        [Range(0, 999),
+        [Label("Bottom Padding"),
+        Range(0, 999),
         DefaultValue(10)]
         public int paddingBottom;
 
-        [Header("Offset")]
+        [Header("[c/ffff00:Offset]")]
 
-        [Range(0, 999),
+        [Label("X"),
+        Tooltip("X position of the tooltip."),
+        Range(0, 999),
         DefaultValue(16)]
         public int x;
 
-        [Range(0, 999),
+        [Label("Y"),
+        Tooltip("Y position of the tooltip."),
+        Range(0, 999),
         DefaultValue(16)]
         public int y;
 
-        [Range(0, 999),
+        [Label("Spacing"),
+        Tooltip("Add extra space between lines."),
+        Range(0, 999),
         DefaultValue(0)]
         public int spacing;
 
-        [Header("Miscellaneous")]
+        [Header("[c/ffff00:Miscellaneous]")]
 
-        [DefaultValue(true)]
+        [Label("Sprite [BROKEN]"),
+        Tooltip("Display the item's sprite in the tooltip. [BROKEN]---Working on a fix"),
+        DefaultValue(true)]
         public bool sprite;
 
-        [DefaultValue(true)]
+        [Label("Text Pulse"), DefaultValue(false)]
         public bool textPulse;
 
-        [DefaultValue(false)]
+        [Label("Crit chance line for ammo"),
+        Tooltip("Off by default because ammo crit chance doesn't affect crit chance."),
+        DefaultValue(false)]
         public bool ammoCrit;
 
-        [Header("VanillaTooltipLines")]
+        [Header("[c/ffff00:Vanilla Tooltip Lines]")]
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("+- X range"),
+        Tooltip("How much more/less range the tool has, in tiles."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color tileBoost;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Ammo"),
+        Tooltip("The item is ammo."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color ammo;
 
-        [DefaultValue(typeof(Color), "190,120,120,255")]
+        [Label("Bad Modifier"), DefaultValue(typeof(Color), "190,120,120,255")]
         public Color badMod;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Can be placed"),
+        Tooltip("The item can be placed."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color placeable;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Consumable"),
+        Tooltip("The item is consumable."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color consumable;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Consumes X"),
+        Tooltip("What item a tile wand consumes."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color wandConsumes;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Damage"),
+        Tooltip("The item's damage and class."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color dmg;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Equipable"),
+        Tooltip("The item is equipable."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color equipable;
 
-        [DefaultValue(typeof(Color), "255,255,255,0")]
+        [Label("Equipped in social slot"),
+        Tooltip("The item is in a social slot."),
+        DefaultValue(typeof(Color), "255,255,255,0")]
         public Color social;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Etherian Mana Warning"),
+        Tooltip("\"Cannot be used without Etherian Mana until the Eternia Crystal has been defended\""),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color etherianMana;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Expert"),
+        Tooltip("The item is from Expert Mode."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color expert;
 
-        [DefaultValue(typeof(Color), "255,255,255,0")]
+        [Label("Favorite Description"),
+        Tooltip("\"Quick trash, stacking, and selling will be blocked\""),
+        DefaultValue(typeof(Color), "255,255,255,0")]
         public Color favDescr;
 
-        [DefaultValue(typeof(Color), "120,190,120,255")]
+        [Label("Good Modifier"), DefaultValue(typeof(Color), "120,190,120,255")]
         public Color goodMod;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Increases life regeneration"),
+        Tooltip("In Expert Mode, shows that the food increases life regeneration."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color wellFedExpert;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Knockback"),
+        Tooltip("The item's knockback."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color knockback;
 
-        [DefaultValue(typeof(Color), "255,255,255,0")]
+        [Label("Marked as favorite"),
+        Tooltip("The item is marked as favorite."),
+        DefaultValue(typeof(Color), "255,255,255,0")]
         public Color fav;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Material"),
+        Tooltip("The item can be used to craft something."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color material;
 
-        [DefaultValue(typeof(Color), "255,255,255,0")]
+        [Label("Quest Item"),
+        Tooltip("The item is a quest item."),
+        DefaultValue(typeof(Color), "255,255,255,0")]
         public Color quest;
 
-        [DefaultValue(typeof(Color), "255,255,255,0")]
+        [Label("Requires bait to catch fish"),
+        Tooltip("The fishing pole requires bait to catch fish."),
+        DefaultValue(typeof(Color), "255,255,255,0")]
         public Color needsBait;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Restores X life"),
+        Tooltip("How much life the player restores when using the item."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color healLife;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Restores X mana"),
+        Tooltip("How much mana the player restores when using the item."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color healMana;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Set Bonus"),
+        Tooltip("The armor's set bonus description."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color setBonus;
 
-        [DefaultValue(typeof(Color), "255,255,255,0")]
+        [Label("Social Description"),
+        Tooltip("\"No stats will be gained\""),
+        DefaultValue(typeof(Color), "255,255,255,0")]
         public Color socialDescr;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Speed"),
+        Tooltip("The item's attack speed."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color speed;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Uses X mana"),
+        Tooltip("How much mana the item consumes when used."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color useMana;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("Vanity Item"),
+        Tooltip("The item is vanity."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color vanity;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X defense"),
+        Tooltip("The item's defense."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color defense;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X minute duration"),
+        Tooltip("The item's buff duration."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color buffTime;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X% axe power"),
+        Tooltip("The item's axe power."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color axePow;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X% bait power"),
+        Tooltip("The bait's bait power."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color baitPow;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X% critical strike chance"),
+        Tooltip("The item's crit chance."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color critChance;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X% fishing power"),
+        Tooltip("The fishing pole's fishing power."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color fishingPow;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X% hammer power"),
+        Tooltip("The item's hammer power."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color hammerPow;
 
-        [DefaultValue(typeof(Color), "255,255,255,255")]
+        [Label("X% pickaxe power"),
+        Tooltip("The item's pickaxe power."),
+        DefaultValue(typeof(Color), "255,255,255,255")]
         public Color pickPow;
     }
 }
